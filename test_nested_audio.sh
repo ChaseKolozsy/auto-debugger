644d489dbd0c3d65cf795b6bb510a0f668035491
Chase Kolozsy <chasekolozsy@gmail.com>
2025-08-11 16:14:30 -0600

    misc edit to test_nested_audio.sh

--- test_nested_audio.sh
+++ test_nested_audio.sh
@@ -29,7 +29,7 @@ echo ""
 python -m autodebugger run \
     --manual \
     --manual-web \
-    --manual-from tests/test_nested_structures.py:24 \
+    --manual-from tests/test_nested_structures.py:48 \
     --manual-audio \
     --manual-rate 210 \
     --stop \
